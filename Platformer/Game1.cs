f18d5705085b30e3059e173d5973bbb269821dd4
SirAkkit <38452138+SirAkkit@users.noreply.github.com>
2019-01-07 11:09:43 +1030

    Added Collision

--- Platformer/Game1.cs
+++ Platformer/Game1.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Graphics;
+using MonoGame.Extended.ViewportAdapters;
+using System;
+
+
 
 namespace Platformer
 {
@@ -9,10 +16,45 @@ namespace Platformer
     /// </summary>
     public class Game1 : Game
     {
+        public static int tile = 64;
+        public static float meter = tile;
+        public static float gravity = meter * 9.8f * 6.0f;
+        public static Vector2 maxVelocity = new Vector2(meter * 10, meter * 15);
+        public static float acceleration = maxVelocity.X * 2;
+        public static float friction = maxVelocity.X * 6;
+        public static float jumpImpulse = meter * 1500;
+
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        Player player = new Player();  
+        Player player = null;
+
+        Camera2D camera = null;
+        TiledMap map = null;
+        TiledMapRenderer mapRenderer = null;
+        TiledMapTileLayer collisionLayer;
+
+        public int ScreenWidth
+        { get
+            {
+                return graphics.GraphicsDevice.Viewport.Width;
+            }
+        }
+
+        public int ScreenHeight
+        {
+            get
+            {
+                return graphics.GraphicsDevice.Viewport.Height;
+            }
+        }
+
+
+
+
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -28,7 +70,7 @@ namespace Platformer
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            player = new Player(this);
             base.Initialize();
         }
 
@@ -42,6 +84,22 @@ namespace Platformer
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             player.Load(Content);
+
+            BoxingViewportAdapter viewportAdapter = new BoxingViewportAdapter(Window,
+            GraphicsDevice,
+            ScreenWidth, ScreenHeight);
+            camera = new Camera2D(viewportAdapter);
+            camera.Position = new Vector2(0, ScreenHeight);
+
+            map = Content.Load<TiledMap>("samp1");
+            mapRenderer = new TiledMapRenderer(GraphicsDevice);
+
+            foreach(TiledMapTileLayer layer in map.TileLayers) {
+                if(layer.Name == "Collisions")
+                {
+                    collisionLayer = layer;
+                }
+            }
 
             // TODO: use this.Content to load your game content here
         }
@@ -62,18 +120,71 @@ namespace Platformer
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                camera.Move(new Vector2(0, -50) * deltaTime);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                camera.Move(new Vector2(0, +50) * deltaTime);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                camera.Move(new Vector2(-50, 0) * deltaTime);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                camera.Move(new Vector2(+50, 0) * deltaTime);
+            }
+
 
             player.Update(deltaTime);
+            camera.Position = player.Position - new Vector2(ScreenWidth / 2, ScreenHeight / 2);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
 
-        /// <summary>
+        public int PixelToTile(float pixelCoord)
+        {
+            return (int)Math.Floor(pixelCoord / tile);
+        }
+
+        public int TileTopixel(int tileCoord)
+        {
+            return tile * tileCoord;
+        }
+
+        public int CellAtPixelCoord(Vector2 PixelCoords)
+        {
+            if (PixelCoords.X < 0 || PixelCoords.X > map.WidthInPixels || PixelCoords.Y < 0)
+                return 1;
+            if (PixelCoords.Y > map.HeightInPixels)
+                return 0;
+            return CellAtTileCoord(PixelToTile(PixelCoords.X), PixelToTile(PixelCoords.Y));
+        }
+
+        public int CellAtTileCoord(int tx, int ty)
+        {
+            if (tx < 0 || tx >= map.Width || ty < 0)
+                return 1;
+            if (ty > map.Height)
+                return 0;
+
+            TiledMapTile? tile;
+            collisionLayer.TryGetTile(tx, ty, out tile);
+            return tile.Value.GlobalIdentifier;
+        }
+
+
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
@@ -82,7 +193,13 @@ namespace Platformer
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            spriteBatch.Begin();
+            var viewMatrix = camera.GetViewMatrix();
+            var projectionMatrix = Matrix.CreateOrthographicOffCenter(0,
+            GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 0, 0f, -1f);
+
+            spriteBatch.Begin(transformMatrix: viewMatrix);
+
+            mapRenderer.Draw(map, ref viewMatrix, ref projectionMatrix);
             player.Draw(spriteBatch);
             spriteBatch.End();
 
--- Platformer/Player.cs
+++ Platformer/Player.cs
@@ -12,11 +12,31 @@ namespace Platformer
 {
     class Player
     {
-        public Vector2 Position = Vector2.Zero;
         Sprite sprite = new Sprite();
-        SpriteEffects effects = SpriteEffects.None;
-        public Player()
+      
+        Game1 game = null;
+        bool isFalling = true;
+        bool isJumping = false;
+
+        Vector2 velocity = Vector2.Zero;
+        Vector2 position = Vector2.Zero;
+
+
+        public Vector2 Position
         {
+            get
+            {
+                return position;
+            }
+        }
+
+        public Player(Game1 game)
+        {
+            this.game = game;
+            isFalling = true;
+            isJumping = false;
+            velocity = Vector2.Zero;
+            position = Vector2.Zero;
         }
 
 
@@ -27,41 +47,108 @@ namespace Platformer
 
         public void Update(float deltaTime) 
         {
+            UpdateInput(deltaTime);
             sprite.Update(deltaTime);
-            KeyboardState State = Keyboard.GetState();
-            int speed = 100;
-            if(State.IsKeyDown(Keys.Up) == true)
-            {
-                Position.Y -= speed * deltaTime;
-                effects = SpriteEffects.None;
-            }
-
-            if (State.IsKeyDown(Keys.Down) == true)
-            {
-                Position.Y += speed * deltaTime;
-                effects = SpriteEffects.FlipVertically;
-            }
-
-            if (State.IsKeyDown(Keys.Left) == true)
-            {
-                Position.X -= speed * deltaTime;
-                effects = SpriteEffects.FlipHorizontally;
-            }
-
-            if (State.IsKeyDown(Keys.Right) == true)
-            {
-                Position.X += speed * deltaTime;
-                effects = SpriteEffects.None;
-                
-            }
 
            
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-                sprite.Draw(spriteBatch, Position, effects);
+                sprite.Draw(spriteBatch, Position);
            
+        }
+
+        private void UpdateInput(float deltaTime)
+        {
+            bool wasMovingLeft = velocity.X < 0;
+            bool wasMovingRight = velocity.X > 0;
+            bool falling = isFalling;
+
+            Vector2 acceleration = new Vector2(0, Game1.gravity);
+
+           
+
+            if(Keyboard.GetState().IsKeyDown(Keys.Left) == true) {
+                acceleration.X -= Game1.acceleration;
+            }
+            else if (wasMovingLeft == true) {
+                acceleration.X += Game1.friction;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) == true)
+            {
+                acceleration.X += Game1.acceleration;
+            }
+            else if (wasMovingRight == true) {
+                acceleration.X -= Game1.friction;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) == true && this.isJumping == false && falling == false)
+            {
+                acceleration.Y -= Game1.jumpImpulse;
+                this.isJumping = true;
+            }
+
+            velocity += acceleration * deltaTime;
+
+            velocity.X = MathHelper.Clamp(velocity.X, -Game1.maxVelocity.X, Game1.maxVelocity.X);
+            velocity.Y = MathHelper.Clamp(velocity.Y, -Game1.maxVelocity.Y, Game1.maxVelocity.Y);
+
+            position += velocity * deltaTime;
+
+            if ((wasMovingLeft && (velocity.X > 0)) || (wasMovingRight && (velocity.X < 0)))
+            {
+                velocity.X = 0;
+            }
+
+            int tx = game.PixelToTile(position.X);
+            int ty = game.PixelToTile(position.Y);
+
+            bool nx = (position.X) % Game1.tile != 0;
+            bool ny = (position.Y) % Game1.tile != 0;
+            bool cell = game.CellAtTileCoord(tx, ty) != 0;
+            bool cellright = game.CellAtTileCoord(tx + 1, ty) != 0;
+            bool celldown = game.CellAtTileCoord(tx, ty + 2) != 0;
+            bool celldiag = game.CellAtTileCoord(tx + 1, ty + 1) != 0;
+
+            if (this.velocity.Y > 0)
+            {
+                if ((celldown && !cell) || (celldiag && !cellright && nx))
+                {
+                    position.Y = game.TileTopixel(ty);
+                    this.velocity.Y = 0;
+                    this.isFalling = false;
+                    this.isJumping = false;
+                    ny = false;
+                }
+            }
+            else if (this.velocity.Y < 0)
+            {
+                if ((cell && !celldown) || (cellright && !celldiag && nx))
+                {
+                    position.Y = game.TileTopixel(ty + 1);
+                    this.velocity.Y = 0;
+                    cell = celldown;
+                    cellright = celldiag;
+                    ny = false;
+                }
+            }
+            if (this.velocity.X > 0)
+            {
+                if ((cellright && !cell) || (celldiag && !celldown && ny))
+                {
+                    position.X = game.TileTopixel(tx);
+                    this.velocity.X = 0;
+                }
+          }
+            else if (this.velocity.X < 0)
+            {
+                if ((cell && !cellright) || (celldown && !celldiag && ny))
+                {
+                    position.X = game.TileTopixel(tx + 1);
+                    this.velocity.X = 0;
+                }
+            }
+            this.isFalling = !(celldown || (nx && celldiag));
         }
     }
 }
